7e7e9785979dde5e782f09a0659caea571c4fbac
Albertoho09 <albertoraikeor@gmail.com>
2024-02-15 01:01:34 +0100

    endpoint comentado

--- src/main/java/com/example/decsecBackend/controladores/userController.java
+++ src/main/java/com/example/decsecBackend/controladores/userController.java
@@ -26,63 +26,71 @@ import com.example.decsecBackend.serviciosImpl.usuarioServicioImpl;
 @RestController
 @RequestMapping("/api/v1/users")
 public class userController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(userController.class);
-	
+
 	@Autowired
 	private usuarioServicioImpl usuarioservice;
-	
-	
-    @GetMapping
-    @PreAuthorize("hasRole('ROLE_USER') || hasRole('ROLE_ADMIN')")
-    public ResponseEntity<?> listarUsuarios(@AuthenticationPrincipal Usuario usuario){
-    	if(usuario.getRoles().contains(Role.ROLE_USER)) {
-    		return ResponseEntity.ok(usuarioservice.listarTodosUsuariosDTO());
-    	}else {
-    		return ResponseEntity.ok(usuarioservice.listarTodosUsuarios());    	}
-    }
-    
-    @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_USER') || hasRole('ROLE_ADMIN')")
-    public ResponseEntity<?> borrarUsuario(@PathVariable(required = false) Long id, @AuthenticationPrincipal Usuario usuario){
-    	if (id != null) {
-    		if(usuario.getRoles().contains(Role.ROLE_ADMIN)) {
-        		if(usuarioservice.existePorId(id)) {
-        			usuarioservice.borrarUsuario(id);
-        	    	return ResponseEntity.status(HttpStatus.OK).build();
-        		}else {
-        	    	return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        		}
-    		}else {
-    			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    		}
-		}else {
-			usuarioservice.borrarUsuario(usuario.getId());
-	    	return ResponseEntity.status(HttpStatus.OK).build();
-		}
-    }
-    
-    @PatchMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_USER') || hasRole('ROLE_ADMIN')")
-    public ResponseEntity<?> actualizarParcialmente(@PathVariable(required = false) Long id, @AuthenticationPrincipal Usuario usuario, @RequestBody Map<String, Object> updates){
-    	if (id != null) {
-    		if(usuario.getRoles().contains(Role.ROLE_ADMIN)) {
-        		if(usuarioservice.existePorId(id)) {
-        			actualizarUsuario(id, updates);
-        		}else {
-        	    	return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        		}
-    		}else {
-    			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    		}
-    	}else {
-			return ResponseEntity.status(HttpStatus.OK).body(actualizarUsuario(usuario.getId(), updates));
-    	}
-    }
 
-	private Usuario actualizarUsuario(Long id, Map<String, Object> updates) {
-		
-		return null;
+	@GetMapping
+	@PreAuthorize("hasRole('ROLE_USER') || hasRole('ROLE_ADMIN')")
+	public ResponseEntity<?> listarUsuarios(@AuthenticationPrincipal Usuario usuario) {
+		if (usuario.getRoles().contains(Role.ROLE_USER)) {
+			return ResponseEntity.ok(usuarioservice.listarTodosUsuariosDTO());
+		} else {
+			return ResponseEntity.ok(usuarioservice.listarTodosUsuarios());
+		}
 	}
-	
+
+	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ROLE_USER') || hasRole('ROLE_ADMIN')")
+	public ResponseEntity<?> borrarUsuario(@PathVariable(required = false) Long id,
+			@AuthenticationPrincipal Usuario usuario) {
+		if (id != null) {
+			if (usuario.getRoles().contains(Role.ROLE_ADMIN)) {
+				if (usuarioservice.existePorId(id)) {
+					usuarioservice.borrarUsuario(id);
+					return ResponseEntity.status(HttpStatus.OK).build();
+				} else {
+					return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+				}
+			} else {
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+			}
+		} else {
+			usuarioservice.borrarUsuario(usuario.getId());
+			return ResponseEntity.status(HttpStatus.OK).build();
+		}
+	}
+
+	/*
+	 * @PatchMapping("/{id}")
+	 * 
+	 * @PreAuthorize("hasRole('ROLE_USER') || hasRole('ROLE_ADMIN')")
+	 * public ResponseEntity<?> actualizarParcialmente(@PathVariable(required =
+	 * false) Long id, @AuthenticationPrincipal Usuario usuario, @RequestBody
+	 * Map<String, Object> updates){
+	 * if (id != null) {
+	 * if(usuario.getRoles().contains(Role.ROLE_ADMIN)) {
+	 * if(usuarioservice.existePorId(id)) {
+	 * actualizarUsuario(id, updates);
+	 * }else {
+	 * return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	 * }
+	 * }else {
+	 * return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	 * }
+	 * }else {
+	 * return
+	 * ResponseEntity.status(HttpStatus.OK).body(actualizarUsuario(usuario.getId(),
+	 * updates));
+	 * }
+	 * }
+	 * 
+	 * private Usuario actualizarUsuario(Long id, Map<String, Object> updates) {
+	 * 
+	 * return null;
+	 * }
+	 */
+
 }
